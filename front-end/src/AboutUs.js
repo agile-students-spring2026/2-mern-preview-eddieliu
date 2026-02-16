acf9f3cd5a48c112b6165532753706cdc4555b84
eddieliu-dev <el3798@nyu.edu>
2026-02-15 23:22:38 -0500

    Added an AboutUs tab with a photo, a bio and a hobby paragraph

---
+++ front-end/src/AboutUs.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import "./AboutUs.css";
+import EddieLiuPhoto from "./image/EddieLiuPhoto.jpg";
+
+const AboutUs = (props) => {
+    return (
+        <>
+            <h1>About Us</h1>
+            <img id="EddieLiuPhoto" src={EddieLiuPhoto} alt="A photo of Eddie Liu"/>
+            <h3 id="EddieLiuName">Eddie Liu</h3>
+            <p id="EddieLiuBio">Eddie Liu is an AI & Backend Developer exploring agentic AI and rapid prototyping while continuing to grow his full-stack skills.</p>
+            <p id="EddieLiuHobbies">When he's not staring at a terminal, he like reading📚, working out💪, playing guitar🎸, or learning new languages🌍.</p>
+        </>
+    )
+}
+
+export default AboutUs;
